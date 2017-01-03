f7e02b6da944885d8b403a84fe062ed43cc35a9a
Imp <imphunter290@gmail.com>
2017-01-03 23:30:20 +0000

    new functions testing of last_action fixed spelling errors attempt to list admins

--- news_network.js
+++ news_network.js
@@ -49,8 +49,6 @@ function(context, args)//list:true
   {
     if(!user)
       return {ok:false,msg:"Enter valid `Nname` to remove to `AINN` admins"}
-    if(! #s.users.last_action({name:user}))
-      return {ok:false,msg:user+" is an invalid name"}
     var data=#db.f({type:"inn_admin_list"}).first();
     if(!data)
       #db.i({type:"inn_admin_list",admins:[]});
@@ -129,8 +127,38 @@ function(context, args)//list:true
 
   if (super_admins.includes(context.caller) && 'super_admin' in args) {
     if(args.super_admin=="add_admin")return AddAdmin(args.user);
-    if(args.uper_admin=="remove_admin")return RemoveAdmin(args.user);
+    if(args.super_admin=="remove_admin")return RemoveAdmin(args.user);
     return admin_header+"\n"+ SuperAdmin().join("\n")
+  }
+
+  let inn_admins=#db.f({main:"inn_admin_list"}).first().admins
+  function MemberList()
+  {
+    let names=inn_admins.join(",").split(",") // this is just making a copy of the list because we are going to mutate it
+    let actions=[]
+    let last_action={};
+    while(names.length>50) {
+      actions.push(...#s.users.last_action({name:names.splice(0,50)}));
+    }
+    actions.push(...#s.users.last_action({name:names}));
+    for(var i=0;i<actions.length;++i) {
+      if(actions[i])
+        last_action[actions[i].n]=new Date(actions[i].t||0)/1
+    }
+    for(var i=0;i<inn_admins.length;++i) {
+      if(!last_action[inn_admins[i]] || _START-last_action[inn_admins[i]]>20*24*3600*1000) {
+          RemoveAdmin(inn_admins[i]]);
+          inn_admins.splice(i,1);
+          --i;
+      }
+    }
+    // let sum = inn_admins.map(p => "`T# " + p + "`").join('\n')
+    // return "`AAdmins of Implink News Network:`\n" + sum
+  }
+  if( (super_admins.includes(context.caller) || inn_admins.includes(context.caller)) && 'admin' in args) {
+    if (args.admin == "members") return MemberList();
+
+    return admin_header+"\n" + Admin().join("\n")
   }
 
   //CORPS FOR INN
