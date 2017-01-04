4c33c8cdc776373f1ea579417329698fd212b3cf
Imp <imphunter290@gmail.com>
2017-01-04 00:38:03 +0000

    added new functions, progress on creating articles

--- news_network.js
+++ news_network.js
@@ -12,11 +12,13 @@ function(context, args)//list:true
   let active = #s.users.active()
   let super_admins=["implink", "imphunter"]
   let lib = #s.scripts.lib()
-
   let admin_header = [
     "     `AINN Admins`",
-    "`c------------------------`"
+    "`c------------------------`",
+    "Command List:\n",
+    "`Ncreate_article` - create a new `AINN` article requires keys: `Nid`:\"<articleid>\", context:\"really long text here\""
   ].join("\n")
+
   function Admin()
   {
     return []
@@ -38,7 +40,7 @@ function(context, args)//list:true
       return {ok:false,msg:"Enter valid `Nname` to add to `AINN` admins"}
     if(! #s.users.last_action({name:user}))
       return {ok:false,msg:user+" is an invalid name"}
-    var data=#db.f({type:"inn_admin_list"}).first();
+    let data=#db.f({type:"inn_admin_list"}).first();
     if(!data)
       #db.i({type:"inn_admin_list",admins:[]});
     #db.u1({type:"inn_admin_list"},{$addToSet:{admins:user}})
@@ -49,13 +51,57 @@ function(context, args)//list:true
   {
     if(!user)
       return {ok:false,msg:"Enter valid `Nname` to remove to `AINN` admins"}
-    var data=#db.f({type:"inn_admin_list"}).first();
+    let data=#db.f({type:"inn_admin_list"}).first();
     if(!data)
       #db.i({type:"inn_admin_list",admins:[]});
     #db.u1({type:"inn_admin_list"},{$pull:{admins:user}})
     return {ok:true, msg:"Admin " + user + " removed to `AINN`."}
   }
 
+  let inn_admins=#db.f({type:"inn_admin_list"}).first().admins
+
+  function MemberList()
+  {
+    let names=inn_admins.join(",").split(",") // this is just making a copy of the list because we are going to mutate it
+    let actions=[]
+    let last_action={};
+    while(names.length>50) {
+      actions.push(...#s.users.last_action({name:names.splice(0,50)}));
+    }
+    actions.push(...#s.users.last_action({name:names}));
+    for(let i=0;i<actions.length;++i) {
+      if(actions[i])
+        last_action[actions[i].n]=new Date(actions[i].t||0)/1
+    }
+    for(let i=0;i<inn_admins.length;++i) {
+      if(!last_action[inn_admins[i]] || _START-last_action[inn_admins[i]]>20*24*3600*1000) {
+          RemoveAdmin(inn_admins[i]);
+          inn_admins.splice(i,1);
+          --i;
+      }
+    }
+    let sum = super_admins.map(p => "`T# " + p + "`").join('\n')
+    let sum2 = inn_admins.map(p => "`T# " + p + "`").join('\n')
+    return "`AINN Admin Member List:`\n`c----------------------`\n`ASUPER:`\n" + sum + "\n`c----------------------`\n`AREGULAR:`\n" + sum2
+  }
+
+  function AddArticle(id, content, title) {
+    if (#db.f({main: "news_network", id: id}).first() != null) return "Article already exists."
+
+    #db.i({
+      main: "news_network",
+      id: id,
+      title: title,
+      voters: [],
+      text: content,
+      uplink: 0,
+      downlink: 0,
+      views: 0,
+      date_uploaded: Date.now(),
+      date_updated: Date.now()
+    })
+    return "Article " + id + " added to `AINN`"
+  }
   if (!args || Object.keys(args).length==0) {
     let banner = [
     "                         `D*UNDER CONSTRUCTION*`",
@@ -68,7 +114,7 @@ function(context, args)//list:true
     "\n                  `AActive Users on hackmud:` " + active,
     "`c------------------------------------------------------------------`",
     "\nhackmud’s most comprehensive news and information publication.",
-    "Featuring a variety of content ranging from up-to-date stories on",
+    "Featuring a letiety of content ranging from up-to-date stories on",
     "current in-game events, to the juiciest gossip on all of your",
     "favourite users. Read interviews with top corp members and content",
     "creators, advertise your services, and stay informed. `AINN`.",
@@ -127,36 +173,24 @@ function(context, args)//list:true
 
   if (super_admins.includes(context.caller) && 'super_admin' in args) {
     if(args.super_admin=="add_admin")return AddAdmin(args.user);
+
     if(args.super_admin=="remove_admin")return RemoveAdmin(args.user);
+
+    if (args.admin=="create") {
+      if (!args.title || !args.id || !args.content) {
+        return "Missing keys. Make sure you have: `Ntitle`, `Nid` and `Ncontent`"
+      }
+      if (typeof args.id !="string" && typeof args.id!="number") { return {ok:false, msg:"`Nid` must be a string or number."}}
+      if (args.id.length = 0) return {ok:false, msg:"Cannot be null."}
+      if (typeof args.title != "string") {return {ok:false, msg:"`Ntitle` must be as string."}}
+      if (args.title.length = 0) return {ok:false, msg:"Cannot be null."}
+      if (typeof args.content != "string") {return {ok:false, msg:"`Ncontent must be string.`"}}
+      if (args.content.length = 0) return {ok:false, msg:"Cannot be null."}
+      return AddArticle(args.title, args.id, args.content)
+    }
     return admin_header+"\n"+ SuperAdmin().join("\n")
   }
 
-  let inn_admins=#db.f({type:"inn_admin_list"}).first().admins
-
-  function MemberList()
-  {
-    let names=inn_admins.join(",").split(",") // this is just making a copy of the list because we are going to mutate it
-    let actions=[]
-    let last_action={};
-    while(names.length>50) {
-      actions.push(...#s.users.last_action({name:names.splice(0,50)}));
-    }
-    actions.push(...#s.users.last_action({name:names}));
-    for(var i=0;i<actions.length;++i) {
-      if(actions[i])
-        last_action[actions[i].n]=new Date(actions[i].t||0)/1
-    }
-    for(var i=0;i<inn_admins.length;++i) {
-      if(!last_action[inn_admins[i]] || _START-last_action[inn_admins[i]]>20*24*3600*1000) {
-          RemoveAdmin(inn_admins[i]);
-          inn_admins.splice(i,1);
-          --i;
-      }
-    }
-    let sum = super_admins.map(p => "`T# " + p + "`").join('\n')
-    let sum2 = inn_admins.map(p => "`T# " + p + "`").join('\n')
-    return "`AINN Admin Member List:`\n`c----------------------`\n`ASUPER:`\n" + sum + "\n`c----------------------`\n`AREGULAR:`\n" + sum2
-  }
   if( (super_admins.includes(context.caller) || inn_admins.includes(context.caller)) && 'admin' in args) {
     if (args.admin == "members") return MemberList();
 
